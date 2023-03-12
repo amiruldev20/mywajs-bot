2efee7a74d9cc11cc46a14937a749fecf8dab368
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-03-12 00:19:02 +0000

    Update ready.js

--- system/event/ready.js
+++ system/event/ready.js
@@ -1,8 +1,3 @@
-import module from 'module'
-global.require = module.createRequire(import.meta.url)
-
-const client = require('../../index');
-
-client.on("ready", async () => {
-    console.log(`BOT READY!!`)
+mywa.on("ready", async () => {
+    console.log(`WWEBJS BOT READY!!`)
 });

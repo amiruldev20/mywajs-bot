1c09382ab456abcf60c9207ab56ae7c69ceee464
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-03-14 22:41:02 +0000

    index • move to esm

--- index.js
+++ index.js
@@ -12,29 +12,38 @@
 | - And you                       |
 |=================================|
 */
+import wweb from 'wwebjs'
 const {
     Client,
     LocalAuth
-} = require('wwebjs')
-const {
+} = wweb
+import {
     Collection
-} = require('@discordjs/collection')
-require('dotenv').config()
-const puppeteer = require("puppeteer")
+} from '@discordjs/collection';
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import cron from 'node-cron';
+import axios from 'axios';
+import handlerMessage from './system/event/msg.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 const mywa = new Client({
     authStrategy: new LocalAuth(),
     qrMaxRetries: 3,
-	takeoverOnConflict: true,
-	takeoverTimeoutMs: 3000,
-	bypassCSP: true,
-	userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    takeoverOnConflict: true,
+    takeoverTimeoutMs: 3000,
+    bypassCSP: true,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     puppeteer: {
-        headless: false,
-        defaultViewport: {
-            width: 800,
-            height: 1288
-        },
-        args: ['--no-sandbox',
+        headless: true,
+        /*   defaultViewport: {
+               width: 800,
+               height: 1288
+           },*/
+        args: [
+            '--no-sandbox',
             '--no-first-run',
             '--no-default-browser-check',
             '--disable-setuid-sandbox',
@@ -43,18 +52,27 @@ const mywa = new Client({
             '--start-maximized'
         ],
         ignoreHTTPSErrors: true,
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-    
+        executablePath: '/usr/bin/google-chrome'
+	    //'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
     }
 });
-// setingan diatas untuk menjalankan puppeteer di rdp
+
+// setingan diatas untuk menjalankan puppeteer di panel (goldpanel lebih tepatnya)
 // run dipanel silahkan true headles
 // panelmu ga sup puppeteer? pakailah goldpanel
-module.exports = mywa;
+
+export default mywa;
 
 mywa.cmd = new Collection();
-require("./system")(mywa);
+(await import("./system/index.js")).default(mywa)
 mywa.initialize();
+
 mywa.on('loading_screen', (percent, message) => {
     console.log('LOADING SCREEN', percent, message);
 });
+
+mywa.on("message_create", async (m) => {
+    if (!m._data.isNewMsg) return
+    //  let m = await serialize(hisoka, message)
+    await handlerMessage(mywa, m)
+})

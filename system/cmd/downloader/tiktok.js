87a8fc4f270d6861447834b2862b71018ed3ddb5
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-03-14 22:50:29 +0000

    Update tiktok.js

--- system/cmd/downloader/tiktok.js
+++ system/cmd/downloader/tiktok.js
@@ -1,41 +1,36 @@
-const axios = require("axios")
-const {
-    MessageMedia
-} = require("wwebjs")
-const fs = require("fs")
-module.exports = {
-    cmd: 'tt',
-    desc: 'tiktok downloader',
-    tags: 'downloader',
-    qt: 'masukan urlnya!!',
-    run: async (myww, m, {
-        mc,
-        text
-    }) => {
-        try {
-        let name = await func.rand(5)
-        m.reply('Downloading...')
-        var res = await axios("https://ssyoutube.com/api/convert", {
-            method: "POST",
-            data: `url=${text}`
-        })
+import axios from "axios";
+import wweb from 'wwebjs'
+const { MessageMedia } = wweb
+import fs from "fs";
 
-        let json = res.data
-        let file = await func.downloadFile(json.url[0].url, name)
-        //  console.log(json)
-        await func.delay(5000)
-        const media = MessageMedia.fromFilePath(`./tmp/${name}.mp4`)
-        // console.log(media)
-        m.reply(media, false, {
-            caption: `*TIKTOK DOWNLOADER*
+export default {
+    cmd: "tt",
+    desc: "tiktok downloader",
+    tags: "downloader",
+    qt: "masukan urlnya!!",
+    run: async (myww, m, { mc, text }) => {
+
+        try {
+            let name = await func.rand(5);
+            m.reply("Downloading...");
+            var res = await axios.post("https://ssyoutube.com/api/convert", `url=${text}`);
+
+            let json = res.data;
+            let file = await func.downloadFile(`${name}.mp4`, json.url[0].url);
+
+            await func.delay(5000);
+
+            const media = MessageMedia.fromFilePath(`./tmp/${name}.mp4`);
+            m.reply(media, false, {
+                caption: `*TIKTOK DOWNLOADER*
 
 Title: ${json.meta.title}
-Duration: ${json.meta.duration}`
-        })
-        fs.unlinkSync(`./tmp/${name}.mp4`)
-    } catch {
-        m.reply(`Permintaan tidak dapat diposes, cobalagi nanti!!`)
-    }
+Duration: ${json.meta.duration}`,
+            });
+            await fs.unlinkSync(`./tmp/${name}.mp4`);
+        } catch {
+            m.reply("Permintaan tidak dapat diproses, cobalagi nanti!!");
+        }
     },
-    isQ: true
-}
+    isQ: true,
+};

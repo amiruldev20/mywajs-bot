b5702836c16f41ce97abe8429c047bc8c6c503e2
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-03-15 01:33:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const mywa = new Client({
     bypassCSP: true,
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     puppeteer: {
-        headless: true,
+        headless: false,
         defaultViewport: {
                width: 800,
                height: 1288

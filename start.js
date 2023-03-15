0f0a5447c2da33cc739e9024bb0be7dd634f7ecc
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-03-15 02:58:10 +0000

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,13 @@
 import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 console.log('Starting . . .');
 
 function start() {
-  let args = [path.join(__dirname, 'src', 'hisoka.js'), ...process.argv.slice(2)];
+  let args = [path.join(__dirname, 'index.js'), ...process.argv.slice(2)];
   let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
     .on('message', data => {
       if (data == 'reset') {

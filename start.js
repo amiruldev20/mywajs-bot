f24fbe6165df5278591276f37e57bee89a8260ef
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-03-15 02:51:06 +0000

    Update start.js

--- start.js
+++ start.js
@@ -5,14 +5,13 @@ import fs from 'fs';
 console.log('Starting . . .');
 
 function start() {
-  let args = [path.join(__dirname, 'src', 'index.js'), ...process.argv.slice(2)];
+  let args = [path.join(__dirname, 'src', 'hisoka.js'), ...process.argv.slice(2)];
   let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
     .on('message', data => {
       if (data == 'reset') {
         console.log('Restarting...');
         p.kill();
         start();
-        delete p;
       }
     })
     .on('exit', code => {
